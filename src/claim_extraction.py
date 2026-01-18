4f13e95298857afa20ba08b6352315a0408cca0a
Sarthak <2sarthaksharma@gmail.com>
2026-01-18 17:26:17 +0530

    feat: add chain to extract claims from document

---
+++ src/claim_extraction.py
@@ -0,0 +1,25 @@
+from src.prompts import extract_claims_prompt
+from src.ingestion import load_pdf
+from langchain_core.documents import Document
+from src.llm_client import get_llm_client
+
+from langchain_core.output_parsers import JsonOutputParser
+
+def extract_claim(docs : list[Document]) -> list[str]:
+    """
+    Takes a list of LangChain Document objects
+    and returns a list of extracted claims (JSON).
+    """
+
+    full_text = "\n".join([doc.page_content for doc in docs])
+
+    llm = get_llm_client()
+    prompt = extract_claims_prompt()
+    parser = JsonOutputParser()
+
+    chain = llm | prompt | parser
+
+    result = chain.invoke({"input" : full_text})
+
+    return result
+   
