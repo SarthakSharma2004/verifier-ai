3d664e46ac55ae34571bb7d7343d8b5e0adf0429
Sarthak <2sarthaksharma@gmail.com>
2026-01-18 17:06:25 +0530

    add prompt for extracting claims

---
+++ src/prompts.py
@@ -0,0 +1,58 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+
+
+def extract_claims_prompt():
+    """This function returns the prompt for extracting claims from text."""
+
+
+    system_prompt = """
+You are a professional in extracting facts from a document.
+You will be given a document and your task is to extract ONLY factual, verifiable claims from text.
+
+A valid claim is:
+
+- A specific number, percentage, or statistic (e.g., "95 % of users prefer X")
+- A date or timeline (e.g., "Founded in 2015", "By 2030")
+- A financial figure (e.g., "Revenue of $2.5M", "Stock price of $150")
+- A named entity fact (e.g., "Apple is the largest tech company")
+- A technical specification (e.g., "Processing speed of 5GB/s")
+- A comparison claim (e.g., "X is 3x faster than Y")
+- Or any other fact that is objectively checkable using reliable sources and is a concrete statement, not an opinion
+
+
+**RULES:**
+
+- Do NOT include opinions, General explanations, subjective statements, or vague statements.
+- Include the exact numbers, dates, and names.
+- Do not hallucinate or make up claims that are not in the document.
+
+
+IMPORTANT:
+- Return results as a structured JSON list ONLY. 
+- Do not add any extra commentary.
+
+
+Return format:
+
+{{
+  "claim": "...",
+  "type": "Category of claim. eg: Financial, Date, Statistics, etc."
+}}
+"""
+
+
+    human_prompt = """
+Extract claims from the following document:
+
+{input}
+"""
+
+
+
+    prompt = ChatPromptTemplate.from_messages([
+            ("system", system_prompt),
+            ("human", human_prompt),
+        ])
+
+    return prompt
