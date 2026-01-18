cac60afc4d1a198231a2b9a71a3f8a1a5665f633
Sarthak <2sarthaksharma@gmail.com>
2026-01-18 17:37:24 +0530

    feat: add tavily as web retriever tool

---
+++ src/web_retriever.py
@@ -0,0 +1,17 @@
+from langchain_tavily import TavilySearch
+import os
+
+
+def get_web_retriever(claim: str):
+    """
+    This function takes a claim string and returns Tavily search results.
+    """
+
+    tool = TavilySearch(
+        max_results = 3,
+        tavily_api_key = os.getenv("TAVILY_API_KEY")
+    )
+
+    results = tool.invoke({"query": claim})
+
+    return results
